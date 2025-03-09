3af00557937ef0eed306b087d781728e687062a8
bryan <bryannov13@gmail.com>
2025-03-08 18:42:17 -0600

    database corrections

--- database/migrations/2025_03_08_135000_create_main_tables_migration.php
+++ database/migrations/2025_03_08_135000_create_main_tables_migration.php
@@ -4,7 +4,9 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateMainTablesMigration extends Migration {
-//Type_person_model 
+	public function up()
+	{
+		//Type_person_model 
 		Schema::create('type_person', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->string('name',100);
@@ -13,32 +15,20 @@ class CreateMainTablesMigration extends Migration {
 			$table->timestamps();
 		});
 
-	}
-	public function down()
-	{
-		Schema::dropIfExists('type_person');
-
-	}
-//Person_model 
+		//Person_model 
 		Schema::create('person', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->string('email',100);
 			$table->string('password',100);
 			$table->unsignedBigInteger('type_id');
-			$table->foreign('type_id')->references('id')->on('Type_person');
+			$table->foreign('type_id')->references('id')->on('type_person');
 			$table->string('username',100);
 			$table->boolean('status')->default(1);
 
 			$table->timestamps();
 		});
 
-	}
-	public function down()
-	{
-		Schema::dropIfExists('person');
-
-	}
-//Category_product_model 
+		//Category_product_model 
 		Schema::create('category_product', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->string('name',100);
@@ -47,38 +37,26 @@ class CreateMainTablesMigration extends Migration {
 			$table->timestamps();
 		});
 
-	}
-	public function down()
-	{
-		Schema::dropIfExists('category_product');
-
-	}
-//Store_model 
+		//Store_model 
 		Schema::create('store', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->string('name',100);
 			$table->string('address',100);
 			$table->unsignedBigInteger('seller');
-			$table->foreign('seller')->references('id')->on('Person');
+			$table->foreign('seller')->references('id')->on('person');
 			$table->boolean('status')->default(1);
 
 			$table->timestamps();
 		});
 
-	}
-	public function down()
-	{
-		Schema::dropIfExists('store');
-
-	}
-//Product_model 
+		//Product_model 
 		Schema::create('product', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->string('name',100);
 			$table->unsignedBigInteger('category');
-			$table->foreign('category')->references('id')->on('Category_product');
+			$table->foreign('category')->references('id')->on('category_product');
 			$table->unsignedBigInteger('Store_id');
-			$table->foreign('Store_id')->references('id')->on('Store');
+			$table->foreign('Store_id')->references('id')->on('store');
 			$table->integer('stock');
 			$table->float('price', 8, 2);
 			$table->string('description',100);
@@ -87,54 +65,36 @@ class CreateMainTablesMigration extends Migration {
 			$table->timestamps();
 		});
 
-	}
-	public function down()
-	{
-		Schema::dropIfExists('product');
-
-	}
-//Cart_model 
+		//Cart_model 
 		Schema::create('cart', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->integer('quantity');
 			$table->unsignedBigInteger('Product_id');
-			$table->foreign('Product_id')->references('id')->on('Product');
+			$table->foreign('Product_id')->references('id')->on('product');
 			$table->unsignedBigInteger('Person_id');
-			$table->foreign('Person_id')->references('id')->on('Person');
+			$table->foreign('Person_id')->references('id')->on('person');
 			$table->boolean('status')->default(1);
 
 			$table->timestamps();
 		});
 
-	}
-	public function down()
-	{
-		Schema::dropIfExists('cart');
-
-	}
-//Orders_model 
+		//Orders_model 
 		Schema::create('orders', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->unsignedBigInteger('Person_id');
-			$table->foreign('Person_id')->references('id')->on('Person');
+			$table->foreign('Person_id')->references('id')->on('person');
 			$table->boolean('status')->default(1);
 
 			$table->timestamps();
 		});
 
-	}
-	public function down()
-	{
-		Schema::dropIfExists('orders');
-
-	}
-//Orders_products_model 
+		//Orders_products_model 
 		Schema::create('orders_products', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->unsignedBigInteger('order_id');
-			$table->foreign('order_id')->references('id')->on('Orders');
+			$table->foreign('order_id')->references('id')->on('orders');
 			$table->unsignedBigInteger('Product_id');
-			$table->foreign('Product_id')->references('id')->on('Product');
+			$table->foreign('Product_id')->references('id')->on('product');
 			$table->integer('quantity');
 			$table->float('price', 8, 2);
 			$table->float('total', 8, 2);
@@ -142,12 +102,17 @@ class CreateMainTablesMigration extends Migration {
 
 			$table->timestamps();
 		});
-
 	}
+
 	public function down()
 	{
 		Schema::dropIfExists('orders_products');
-
+		Schema::dropIfExists('orders');
+		Schema::dropIfExists('cart');
+		Schema::dropIfExists('product');
+		Schema::dropIfExists('store');
+		Schema::dropIfExists('category_product');
+		Schema::dropIfExists('person');
+		Schema::dropIfExists('type_person');
 	}
-
 }
